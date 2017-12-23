437c395b5cb74b63714c251023b5d7ce31cc83ea
miaoxu9999 <2229009552@qq.com>
2017-12-23 16:46:09 +0800

    add some property

--- angular/src/app/member/member.ts
+++ angular/src/app/member/member.ts
@@ -1,8 +1,15 @@
 export class Member {
   memberid: number;
+  name: any;
   studentID: string;
   password: string;
-  memberDetail: any;
+  // memberDetail: any;
   privilegeType: number;
-  registration_temps: any;
+  // registration_temps: any;
+
+  tjsq: any;//提交入党申请书的日期
+  rdjjfz: any;//称为 入党积极分子的日期
+  fzdx: any; //成为 发展对象的日期
+  rdrq: any; // 入党日期
+
 }
